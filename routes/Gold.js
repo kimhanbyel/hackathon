de801131e1be58b9d1c0f7264aba0d3f5758084b
kimhanbyel <cjstk1235426@gmail.com>
2023-07-20 14:59:03 +0900

    김한별 Coin 제작

--- routes/Gold.js
+++ routes/Gold.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const goldHandler = require('../handlers/Gold.js');
 
-
 router.get('/', (req,res) => {
     res.send('금')});
 module.exports = router; 
